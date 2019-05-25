2d22f7b275a52fd57e9d465e61eaa3f3cd5207cb
wickdeeb <isak.m.h.s@gmail.com>
2019-05-25 17:08:41 +0200

    about, landing and skills added

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+  return(
+    <div className="about-container">
+      <h1>About Me</h1>
+      <div className="about-text">
+        ...
+        <br />
+        <br />
+        ...
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Landing() {
+  return(
+    <div className="landing-container">
+      <div className="landing-items">
+        <div className="landing-name">
+          Isak Solheim
+        </div>
+        <div className="landing-undertitle">
+          Web Developer
+        </div>
+        <div className="landing-button">
+          My Projects
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
