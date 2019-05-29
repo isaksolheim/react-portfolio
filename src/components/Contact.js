10400c0a117be8539981f26ab483232b92dcab87
wickdeeb <isak.m.h.s@gmail.com>
2019-05-29 12:35:52 +0200

    styled form and added footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,14 @@ import React from 'react';
 function Contact() {
   return(
     <div className="contact-container" id="contact">
-      <h1>Contact</h1>
+      <h1 id="title">Contact</h1>
       <form action="https://formspree.io/spamisak@gmail.com" method="POST">
-        <input type="text" name="name" />
-        <input type="email" name="_replyto" />
+        <label>Email</label>
+        <input type="email" name="_replyto" placeholder="your@email.com" />
+
+        <label>Message</label>
+        <textarea type="text" name="message" placeholder="Hi..."></textarea>
+        
         <input type="submit" value="Send" />
       </form> 
     </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <div className="footer-container">
+      <a href="https://github.com/wickdeeb"><i className="fab fa-3x fa-github" /></a>
+      <br />
+      <br />
+      Isak Solheim 2019
+    </div>
+  );
+}
+
+export default Footer;
