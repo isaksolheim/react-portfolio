051101f3d7acf1b1cf304d203aca9b6d777467c7
wickdeeb <isak.m.h.s@gmail.com>
2019-07-06 16:32:59 +0200

    added validation to from

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,48 @@
 import React from 'react';
 
-function Contact() {
-  return(
-    <div className="contact-container" id="contact">
-      <h1 id="title">Contact</h1>
-      <form action="https://formspree.io/isak.m.h.s@gmail.com" method="POST">
-        <label>Email</label>
-        <input type="email" name="_replyto" placeholder="your@email.com" />
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <label>Message</label>
-        <textarea type="text" name="message" placeholder="Hi..."></textarea>
-        
-        <input type="submit" value="Send" />
-      </form> 
-      <br />
-      <br />
-      <div className="email">
-        <p>My Email Adress:</p>
-        <a href="mailto:isak.m.h.s@gmail.com"><p><strong>isak.m.h.s@gmail.com</strong></p></a>
-        <p><a href={'Isak-Solheim-Front-End-CV.pdf'} download>My Resume</a></p>
+    this.state = {
+      errors: []
+    }
+  }
+  
+  handleSubmit = (e) => {
+    var email = document.getElementById('email').value;
+    var text = document.getElementById('text').value;
+
+    if (email.length === 0 || text.length === 0) {
+      e.preventDefault();
+      document.getElementById('error').innerHTML = "Make sure to enter an email and a message.";
+    }
+  }
+
+  render() {
+    return(
+      <div className="contact-container" id="contact">
+        <h1 id="title">Contact</h1>
+        <form onClick={this.handleSubmit} action="https://formspree.io/isak.m.h.s@gmail.com" method="POST">
+          <label>Email</label>
+          <input id="email" type="email" name="_replyto" placeholder="your@email.com" />
+
+          <label>Message</label>
+          <textarea id="text" type="text" name="message" placeholder="Hi..."></textarea>
+          
+          <input type="submit" value="Send" />
+        </form> 
+        <p id="error"></p>
+        <br />
+        <br />
+        <div className="email">
+          <p>My Email Adress:</p>
+          <a href="mailto:isak.m.h.s@gmail.com"><p><strong>isak.m.h.s@gmail.com</strong></p></a>
+          <p><a href={'Isak-Solheim-Front-End-CV.pdf'} download>My Resume</a></p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contact;
